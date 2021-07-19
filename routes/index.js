fbbbe91a83b2dd7c5d826bd530ff94b98e0b9e0d
fbs11 <46707393+fbs11@users.noreply.github.com>
2021-07-19 14:26:59 +0200

    Initial author routes

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ router.get('/', (req, res) => {
     res.render('index')
 })
 
+//5YG5pQSoDUOAhCso
+
 module.exports = router
+
+//heroku config:set DATABASE_URL="mongodb+srv://user:5YG5pQSoDUOAhCso@cluster0.ejnxh.mongodb.net/test?retryWrites=true&w=majority"
