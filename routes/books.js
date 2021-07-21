e312cb9f015691517cddf45f0ecedaf6c0f2016c
fbs11 <46707393+fbs11@users.noreply.github.com>
2021-07-21 12:41:03 +0200

    File Upload Database instead of server

--- routes/books.js
+++ routes/books.js
@@ -5,14 +5,9 @@ const fs = require('fs')
 const Book = require('../models/book')
 const Author = require('../models/author')
 const path = require('path')
-const uploadPath = path.join('public', Book.coverImageBasePath)
+
 const imagenMimeTypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']
-const upload = multer({
-    dest: uploadPath,
-    fileFilter: (req, file, callback) => {
-        callback(null, imagenMimeTypes.includes(file.mimetype))
-    }
-})
+
 
 //All books route
 router.get('/', async (req, res) => {
@@ -45,17 +40,19 @@ router.get('/new', async (req, res) => {
 })
 
 //Create book route
-router.post('/', upload.single('cover'), async (req, res) => {
+router.post('/', async (req, res) => {
     const fileName = req.file != null ? req.file.filename : null
     const book = new Book({
         title: req.body.title,
         author: req.body.author,
         publishDate: new Date(req.body.publishDate),
         pageCount: req.body.pageCount,
-        coverImageName: fileName,
-        description: req.body.description,
 
+        description: req.body.description,
     })
+
+    saveCover(book, req.body.cover)
+
     try {
         const newBook = await book.save()
         res.redirect(`books`)
@@ -73,6 +70,15 @@ function removeBookCover(fileName) {
     fs.unlink(path.join(uploadPath, fileName), err => {
         if (err) console.error(err)
     })
+}
+
+function saveCover(book, coverEncoded) {
+    if (coverEncoded == null) return
+    const cover = JSON.parse(coverEncoded)
+    if (cover != null && imagenMimeTypes.includes(cover.type)) {
+        book.coverImage = new Buffer.from(cover.data, 'base64')
+        book.coverImageType = cover.type
+    }
 }
 
 async function renderNewPage(res, book, hasError = false) {
